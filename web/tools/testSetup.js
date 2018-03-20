149a76a98c5afc8c626e3de658c6fecfcc6a4833
Michele Mendel <michelemendel@gmail.com>
2018-03-20 12:31:27 +0100

    Commit before jesting

--- web/tools/testSetup.js
+++ web/tools/testSetup.js
@@ -9,41 +9,41 @@
 
 
 // This assures the .babelrc dev config (which includes hot module reloading code) doesn't apply for tests.
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
 // Register babel so that it will transpile ES6 to ES5 before our tests run.
-require('babel-register')();
+require("babel-register")();
 
 // Disable webpack-specific features for tests since
-// Mocha doesn't know what to do with them.
-require.extensions['.css'] = function () {return null;};
-require.extensions['.png'] = function () {return null;};
-require.extensions['.jpg'] = function () {return null;};
+// Mocha doesn"t know what to do with them.
+require.extensions[".css"] = function () {return null;};
+require.extensions[".png"] = function () {return null;};
+require.extensions[".jpg"] = function () {return null;};
 
-let exposedProperties = ['window', 'navigator', 'document'];
+let exposedProperties = ["window", "navigator", "document"];
 
 // Configure JSDOM and set global variables to simulate a browser environment for tests.
-const jsdom = require('jsdom');
+const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
-const {document} = (new JSDOM('')).window;
+const {document} = (new JSDOM("")).window;
 
 global.document = document;
 global.window = document.defaultView;
 
 Object.keys(document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
+    if (typeof global[property] === "undefined") {
         exposedProperties.push(property);
         global[property] = document.defaultView[property];
     }
 });
 
 global.navigator = {
-    userAgent: 'node.js'
+    userAgent: "node.js"
 };
 
 documentRef = document;  //eslint-disable-line no-undef
 
 // Configure adapter for react
 const enzyme = require("enzyme");
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter = require("enzyme-adapter-react-16");
 enzyme.configure({ adapter: new Adapter() });
